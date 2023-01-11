62668983f65d7959ef0ad31f00eed0df1bbc727e
Yago Cunha <111362219+yagocunha990@users.noreply.github.com>
2023-01-11 07:30:27 -0300

    2.09.43

--- pontodevenda/eComm-en/resources/views/cartlist.blade.php
+++ pontodevenda/eComm-en/resources/views/cartlist.blade.php
@@ -1,31 +1,48 @@
 @extends('master')
-@section("content")
-<div class="custom-product">
-     <div class="col-sm-10">
-        <div class="trending-wrapper">
-            <h4>Result for Products</h4>
-            <a class="btn btn-success" href="ordernow">Order Now</a> <br> <br>
-            @foreach($products as $item)
-            <div class=" row searched-item cart-list-devider">
-             <div class="col-sm-3">
-                <a href="detail/{{$item->id}}">
-                    <img class="trending-image" src="{{$item->gallery}}">
-                  </a>
-             </div>
-             <div class="col-sm-4">
-                    <div class="">
-                      <h2>{{$item->name}}</h2>
-                      <h5>{{$item->description}}</h5>
-                    </div>
-             </div>
-             <div class="col-sm-3">
-                <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning" >Remove to Cart</a>
-             </div>
-            </div>
-            @endforeach
-          </div>
-          <a class="btn btn-success" href="ordernow">Order Now</a> <br> <br>
+@section('content')
+<div class="  custom-product">
 
-     </div>
+
+
+    <div class="col-sm-10">
+
+
+
+                <div class="trending-wrapper">
+
+                    <h4>Resultado dos Produtos</h4>
+                    @foreach ($products as $product )
+                             <div class=" row searched-item ">
+                                    <div class="col-sm-3">
+                                        <a href="detail/{{$product->id  }}">
+                                            <img src="{{ $product->gallery }}" class="trending-image" alt="...">
+
+                                    </a>
+                                    </div>
+
+                                    <div class="col-sm-4">
+                                        <a href="detail/{{$product->id  }}">
+
+                                            <div class="">
+                                            <h2>{{ $product->name }}</h2>
+                                            <h5>{{ $product->description }}</h5>
+
+                                            </div>
+                                    </a>
+                                    </div>
+
+                                    <div class="col-sm-3">
+                                       <button class="btn btn-warning">Remover do Carrinho</button>
+                                    </div>
+
+
+                                </div>
+                    @endforeach
+                    </div>
+
+                </div>
+
 </div>
-@endsection 
+
+@endsection
+
